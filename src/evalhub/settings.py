9b09e31e431ad4ce2c9c95ecbccea263895312f2
Laura Gates <lauragates1214@users.noreply.github.com>
2025-09-27 11:33:44 +0100

    Environment variables set up

--- src/evalhub/settings.py
+++ src/evalhub/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 import os
 
@@ -19,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i1h0dt0-m=sel8iw_&xtb*@cc+vjo3g5%+o&!qd)v@pik(tn^a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if "DJANGO_DEBUG_FALSE" in os.environ:
