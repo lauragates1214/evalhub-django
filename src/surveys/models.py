97a97da48061138afd4f6043cdd9a6f839c70b0b
Laura Gates <lauragates1214@users.noreply.github.com>
2025-10-05 16:23:35 +0100

    Survey.name property returns first question text

--- src/surveys/models.py
+++ src/surveys/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.conf import settings
 from django.urls import reverse
 
 
 class Survey(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField(default="")
+
+    @property
+    def name(self):
+        return self.question_set.first().text
 
     def get_absolute_url(self):
         return reverse("view_survey", args=[self.id])
--- src/surveys/tests/test_models.py
+++ src/surveys/tests/test_models.py
@@ -8,31 +8,37 @@ from surveys.models import Question, Survey
 
 
 class QuestionModelsTest(TestCase):
+    def setUp(self):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+        self.user = User.objects.create(email="test@example.com")
+
     def test_default_text(self):
         question = Question()
         self.assertEqual(question.text, "")
 
     def test_question_is_related_to_survey(self):
-        mysurvey = Survey.objects.create()
+        mysurvey = Survey.objects.create(owner=self.user)
         question = Question()
         question.survey = mysurvey
         question.save()
         self.assertIn(question, mysurvey.question_set.all())
 
     def test_cannot_save_null_questions(self):
-        mysurvey = Survey.objects.create()
+        mysurvey = Survey.objects.create(owner=self.user)
         question = Question(survey=mysurvey, text=None)
         with self.assertRaises(IntegrityError):
             question.save()  # database-level validation
 
     def test_cannot_save_empty_questions(self):
-        mysurvey = Survey.objects.create()
+        mysurvey = Survey.objects.create(owner=self.user)
         question = Question(survey=mysurvey, text="")
         with self.assertRaises(ValidationError):
             question.full_clean()  # model-level validation
 
     def test_duplicate_questions_are_invalid(self):
-        mysurvey = Survey.objects.create()
+        mysurvey = Survey.objects.create(owner=self.user)
         Question.objects.create(survey=mysurvey, text="why")
         with self.assertRaises(ValidationError):
             question = Question(survey=mysurvey, text="why")
@@ -40,21 +46,27 @@ class QuestionModelsTest(TestCase):
 
     # TODO: when expand, remove this test as will have org-wide question bank
     def test_CAN_save_same_question_to_different_surveys(self):
-        survey1 = Survey.objects.create()
-        survey2 = Survey.objects.create()
+        survey1 = Survey.objects.create(owner=self.user)
+        survey2 = Survey.objects.create(owner=self.user)
         Question.objects.create(survey=survey1, text="why")
         question = Question(survey=survey2, text="why")
         question.full_clean()  # should not raise
 
 
 class SurveyModelTest(TestCase):
+    def setUp(self):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+        self.user = User.objects.create(email="test@example.com")
+
     def test_get_absolute_url(self):
-        mysurvey = Survey.objects.create()
+        mysurvey = Survey.objects.create(owner=self.user)
 
         self.assertEqual(mysurvey.get_absolute_url(), f"/surveys/{mysurvey.id}/")
 
     def test_survey_questions_order(self):
-        survey1 = Survey.objects.create()
+        survey1 = Survey.objects.create(owner=self.user)
         question1 = Question.objects.create(survey=survey1, text="i1")
         question2 = Question.objects.create(survey=survey1, text="question 2")
         question3 = Question.objects.create(survey=survey1, text="3")
@@ -72,3 +84,13 @@ class SurveyModelTest(TestCase):
     def test_string_representation(self):
         question = Question(text="some text")
         self.assertEqual(str(question), "some text")
+
+    def test_get_absolute_url(self):
+        survey = Survey.objects.create(owner=self.user)
+        self.assertEqual(survey.get_absolute_url(), f"/surveys/{survey.id}/")
+
+    def test_survey_name_is_first_question_text(self):
+        survey = Survey.objects.create(owner=self.user)
+        Question.objects.create(survey=survey, text="First question")
+        Question.objects.create(survey=survey, text="Second question")
+        self.assertEqual(survey.name, "First question")
