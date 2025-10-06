86bfc5bb82e90bb7b662dad95cc7abe9f234f641
Laura Gates <lauragates1214@users.noreply.github.com>
2025-10-06 11:23:05 +0100

    Fix syntax error @skipIf decorator positioning

--- src/functional_tests/test_survey_question_validation.py
+++ src/functional_tests/test_survey_question_validation.py
@@ -81,9 +81,8 @@ class QuestionValidationTest(FunctionalTest):
         self.wait_for_row_in_question_table("1: Is a capybara?")
         self.wait_for_row_in_question_table("2: Why capybara?")
 
+    @skipIf(os.environ.get("CI"), "UX enhancement testing not reliable in headless CI")
     def test_error_messages_are_cleared_on_input(self):
-        @skipIf(os.environ.get("CI"), "UX enhancement testing not reliable in headless CI")
-
         # User 1 logs in
         self.login("user@example.com")
 
