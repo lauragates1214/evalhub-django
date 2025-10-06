fa21e3c3050be462d3511f2ac9b73deedbeb867e
Laura Gates <lauragates1214@users.noreply.github.com>
2025-10-06 10:21:15 +0100

    Add screenshot capture on test failure

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -1,17 +1,17 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-
 import os
 import time
+from datetime import datetime
+from pathlib import Path
 from unittest import skip
 
 from .container_commands import reset_database
 
 MAX_WAIT = 5
+SCREEN_DUMP_LOCATION = Path(__file__).absolute().parent / "screendumps"
 
 
 def wait(fn):
@@ -29,6 +29,7 @@ def wait(fn):
 
 
 class FunctionalTest(StaticLiveServerTestCase):
+    ### Helper methods for functional tests ###
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.test_server = os.environ.get("TEST_SERVER")
@@ -37,9 +38,33 @@ class FunctionalTest(StaticLiveServerTestCase):
             reset_database(self.test_server)
 
     def tearDown(self):
+        if self._test_has_failed():
+            if not SCREEN_DUMP_LOCATION.exists():
+                SCREEN_DUMP_LOCATION.mkdir(parents=True)
+            self.take_screenshot()
+            self.dump_html()
         self.browser.quit()
 
-    ### Helper methods ###
+    def _test_has_failed(self):
+        return self._outcom
+
+    # helper methods for taking screenshots and dumping HTML
+    def take_screenshot(self):
+        path = SCREEN_DUMP_LOCATION / self._get_filename("png")
+        print("screenshotting to", path)
+        self.browser.get_screenshot_as_file(str(path))
+
+    def dump_html(self):
+        path = SCREEN_DUMP_LOCATION / self._get_filename("html")
+        print("dumping page HTML to", path)
+        path.write_text(self.browser.page_source)
+
+    def _get_filename(self, extension):
+        timestamp = datetime.now().isoformat().replace(":", ".")[:19]
+        return (
+            f"{self.__class__.__name__}.{self._testMethodName}-{timestamp}.{extension}"
+        )
+
     def login(self, email, password="password"):
         from django.contrib.auth import get_user_model
         from selenium.webdriver.common.keys import Keys
