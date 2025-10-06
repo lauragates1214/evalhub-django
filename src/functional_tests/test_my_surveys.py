0e3e991994c835f04a6b5ab2fdd170239397f942
Laura Gates <lauragates1214@users.noreply.github.com>
2025-10-06 10:47:12 +0100

    Refactor MySurveysTest to use real login instead of pre-authenticated sessions

--- src/functional_tests/test_my_surveys.py
+++ src/functional_tests/test_my_surveys.py
@@ -1,44 +1,14 @@
-from django.conf import settings
-from django.contrib.auth import (
-    BACKEND_SESSION_KEY,
-    HASH_SESSION_KEY,
-    SESSION_KEY,
-    get_user_model,
-)
-
 from selenium.webdriver.common.by import By
 
 from .base import FunctionalTest
-from .container_commands import create_session_on_server
-from .management.commands.create_session import (
-    create_pre_authenticated_session as create_session_locally,
-)
-
-User = get_user_model()
 
 
 class MySurveysTest(FunctionalTest):
-    def create_pre_authenticated_session(self, email):
-        if self.test_server:
-            session_key = create_session_on_server(self.test_server, email)
-        else:
-            session_key = create_session_locally(email)
-
-        # Set cookie in browser by visiting an arbitrary page first (404 loads fastest)
-        self.browser.get(self.live_server_url + "/404_no_such_url/")
-        self.browser.add_cookie(
-            dict(
-                name=settings.SESSION_COOKIE_NAME,
-                value=session_key,
-                path="/",
-            )
-        )
-
     def test_logged_in_users_surveys_are_saved(self):
         email = "user@example.com"
 
-        # User1 is a logged-in user
-        self.create_pre_authenticated_session(email)
+        # User1 logs in
+        self.login(email)
 
         # They go to home page and create a survey
         self.browser.get(self.live_server_url)
@@ -83,7 +53,7 @@ class MySurveysTest(FunctionalTest):
             lambda: self.assertEqual(self.browser.current_url, second_survey_url)
         )
 
-        # They log out.  The "My Surveys" option disappears
+        # They log out. The "My Surveys" option disappears
         self.browser.find_element(By.CSS_SELECTOR, "#id_logout").click()
         self.wait_for(
             lambda: self.assertEqual(
